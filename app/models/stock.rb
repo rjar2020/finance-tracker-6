5277d10b44d63c654936272ea5ace0672de9e3b5
rjar2020 <rolando.angelini@factorial.co>
2022-12-24 12:20:00 +0100

    Implementing Stock model using IEX client

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:api_key],
+      endpoint: Rails.application.credentials.iex_client[:api_url],
+    )
+    client.quote(ticker_symbol)
+  end
 end
